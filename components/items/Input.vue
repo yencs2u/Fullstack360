6bec4cee4adfbe5fe33de4709f1baf420f5e833d
yencs2u <66502930+yencs2u@users.noreply.github.com>
2020-06-07 11:20:02 +0800

    1st commit

---
+++ components/items/Input.vue
@@ -0,0 +1,44 @@
+<template>
+
+</template>
+<script>
+export default {
+    props:{
+        labelName: String,
+        labelId: String,
+        inputType: String,
+        model: String,
+        disabled: Boolean,
+    },
+    computed:{
+        inputModel: {
+            get(){
+                return this.model;
+            },
+            set(val){
+                this.$emit('input', val);
+            }
+        }
+    }
+}
+</script>
+<style>
+.input {
+        transition: border 0.2s ease-in-out;
+    }
+
+.input:focus+.label,
+.input:active+.label,
+.input.filled+.label {
+    font-size: .55rem;
+    transition: all 0.2s ease-out;
+    top: -0.3rem;
+    color: #323232;
+}
+
+.label {
+    transition: all 0.2s ease-out;
+    top: 0.2rem;
+    left: 0;
+}
+</style>
